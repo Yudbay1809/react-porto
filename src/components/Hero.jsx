b29220703a4ae58f78ff4684e1d446eb3c59cb7b
Yudha Bayu Prastyo <88624834+Yudbay1809@users.noreply.github.com>
2025-09-18 12:56:26 +0700

    tambahan 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                     initial="hidden"
                     animate="visible"
                     className=" pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-                    Yudha Bayu Pratyo
+                    Yudha Bayu Prastyo
                     </motion.h1>
                     <motion.span 
                     variants={container(0.5)}

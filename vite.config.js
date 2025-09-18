4274b4a39cf900917c98bf94e0a7e1e5ae632739
Yudha Bayu Prastyo <88624834+Yudbay1809@users.noreply.github.com>
2025-09-18 12:11:06 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NAMA-REPO-GITHUB/', // Ganti dengan nama repo kamu
+  base: '/react-porto/', // Ganti dengan nama repo kamu
 })
